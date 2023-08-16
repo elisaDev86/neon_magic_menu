bcf5738a7a6c9a22294f27f636d50a433ad4bd21
ElisaDev <140317344+elisaDev86@users.noreply.github.com>
2023-08-16 15:19:28 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li><a href='#'>Home</a></li>
+          <li><a href='#'>About</a></li>
+          <li><a href='#'>Service</a></li>
+          <li><a href='#'>Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
